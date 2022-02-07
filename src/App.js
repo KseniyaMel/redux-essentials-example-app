4ccc774f33c2f4150720e620b43b95684fa6d79b
Kseniya <kcniyamelihova@gmail.com>
2022-02-07 14:34:22 +0300

    added notifications

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom'
 
 import { Navbar } from './app/Navbar'
+import NotificationsList from './features/notifications/NotificationsList'
 import { AddPostForm } from './features/posts/AddPostForm'
 import { EditPostForm } from './features/posts/EditPostForm'
 import PostsList from './features/posts/PostsList'
@@ -20,6 +21,7 @@ function App() {
       <Navbar />
       <div className="App">
         <Switch>
+          <Route exact path="/notifications" component={NotificationsList} />
           <Route exact path="/">
             <AddPostForm />
             <PostsList />
@@ -28,6 +30,7 @@ function App() {
           <Route exact path="/editPost/:postId" component={EditPostForm} />
           <Route exact path="/users" component={UsersList} />
           <Route exact path="/users/:userId" component={UserPage} />
+
           <Redirect to="/" />
         </Switch>
       </div>
